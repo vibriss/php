0af4bddc3f1468b7397a0838c1ee644b51716853
vibriss <61240396+vibriss@users.noreply.github.com>
2020-03-18 16:53:52 +0400

    Delete upload.php

--- upload.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="script.php" enctype="multipart/form-data">
-    введите текст:<br>
-    <input type="text" name="text" />
-    <button type="submit">загрузить</button>
-</form>
